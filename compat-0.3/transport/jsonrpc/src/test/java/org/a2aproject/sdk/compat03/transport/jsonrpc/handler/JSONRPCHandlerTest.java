0aac6eb37ca3940a2a33a89495b730435710cb50
Kabir Khan <kkhan@redhat.com>
2026-04-10 16:29:20 +0200

    Add the transport/* modules
    Comment out usage of DefaultRequestHandler until we have the payload conversion.
    Disable tests since the code is not runnable yet

---
+++ compat-0.3/transport/jsonrpc/src/test/java/org/a2aproject/sdk/compat03/transport/jsonrpc/handler/JSONRPCHandlerTest.java
@@ -0,0 +1,17 @@
+package org.a2aproject.sdk.compat03.transport.jsonrpc.handler;
+
+import org.junit.jupiter.api.Disabled;
+
+// TODO: Uncomment entire test class when server-common is ported
+// This file has been temporarily stubbed out because it depends on server-common classes
+// that haven't been ported yet. See the original at:
+// /Users/kabir/sourcecontrol/AI/a2a-java-0.3.x/transport/jsonrpc/src/test/java/io/a2a/transport/jsonrpc/handler/JSONRPCHandlerTest.java
+
+/**
+ * Placeholder stub for JSONRPCHandlerTest.
+ * The full implementation is commented out until server-common module is ported.
+ */
+@Disabled("Disabled until server-common is ported")
+public class JSONRPCHandlerTest {
+    // Full test implementation commented out - awaiting server-common port
+}
---
+++ compat-0.3/transport/rest/src/main/java/org/a2aproject/sdk/compat03/transport/rest/handler/RestHandler.java
@@ -0,0 +1,14 @@
+package org.a2aproject.sdk.compat03.transport.rest.handler;
+
+// TODO: Uncomment entire implementation when server-common is ported
+// This file has been temporarily stubbed out because it depends on server-common classes
+// that haven't been ported yet. See the original at:
+// /Users/kabir/sourcecontrol/AI/a2a-java-0.3.x/transport/rest/src/main/java/io/a2a/transport/rest/handler/RestHandler.java
+
+/**
+ * Placeholder stub for RestHandler.
+ * The full implementation is commented out until server-common module is ported.
+ */
+public class RestHandler {
+    // Full implementation commented out - awaiting server-common port
+}
