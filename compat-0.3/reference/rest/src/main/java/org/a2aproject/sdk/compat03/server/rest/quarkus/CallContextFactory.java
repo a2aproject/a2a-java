af08a558bf26cd6ea15932f3a10d018b5955173c
Kabir Khan <kkhan@redhat.com>
2026-04-13 11:16:43 +0200

    Implement rest handlers

--- compat-0.3/reference/rest/src/main/java/org/a2aproject/sdk/compat03/server/rest/quarkus/CallContextFactory.java
+++ compat-0.3/reference/rest/src/main/java/org/a2aproject/sdk/compat03/server/rest/quarkus/CallContextFactory.java
@@ -1,10 +1,8 @@
 package org.a2aproject.sdk.compat03.server.rest.quarkus;
 
-// TODO: Uncomment when server-common is ported
-// See: /Users/kabir/sourcecontrol/AI/a2a-java-0.3.x/reference/rest/src/main/java/io/a2a/server/rest/quarkus/CallContextFactory.java
+import org.a2aproject.sdk.server.ServerCallContext;
+import io.vertx.ext.web.RoutingContext;
 
-/**
- * Placeholder stub - awaiting server-common port.
- */
-public class CallContextFactory {
+public interface CallContextFactory {
+    ServerCallContext build(RoutingContext rc);
 }
