97da95144f00269c51d39377b1a95d6e8a2972b2
Kabir Khan <kkhan@redhat.com>
2026-04-13 16:51:01 +0200

    Wire converting request handler into GrpcHandler

--- compat-0.3/reference/grpc/src/main/java/org/a2aproject/sdk/compat03/server/grpc/quarkus/QuarkusGrpcHandler.java
+++ compat-0.3/reference/grpc/src/main/java/org/a2aproject/sdk/compat03/server/grpc/quarkus/QuarkusGrpcHandler.java
@@ -8,11 +8,11 @@ import jakarta.inject.Inject;
 import io.quarkus.grpc.GrpcService;
 import io.quarkus.grpc.RegisterInterceptor;
 import io.quarkus.security.Authenticated;
+import org.a2aproject.sdk.compat03.conversion.Convert03To10RequestHandler;
 import org.a2aproject.sdk.compat03.spec.AgentCard;
 import org.a2aproject.sdk.compat03.transport.grpc.handler.CallContextFactory;
 import org.a2aproject.sdk.compat03.transport.grpc.handler.GrpcHandler;
 import org.a2aproject.sdk.server.PublicAgentCard;
-import org.a2aproject.sdk.server.requesthandlers.RequestHandler;
 import org.a2aproject.sdk.server.util.async.Internal;
 
 @GrpcService
@@ -21,26 +21,19 @@ import org.a2aproject.sdk.server.util.async.Internal;
 public class QuarkusGrpcHandler extends GrpcHandler {
 
     private final AgentCard agentCard;
-    private final RequestHandler requestHandler;
     private final Instance<CallContextFactory> callContextFactoryInstance;
     private final Executor executor;
 
     @Inject
     public QuarkusGrpcHandler(@PublicAgentCard AgentCard agentCard,
-                              RequestHandler requestHandler,
+                              Convert03To10RequestHandler requestHandler,
                               Instance<CallContextFactory> callContextFactoryInstance,
                               @Internal Executor executor) {
         this.agentCard = agentCard;
-        this.requestHandler = requestHandler;
         this.callContextFactoryInstance = callContextFactoryInstance;
         this.executor = executor;
+        setRequestHandler(requestHandler);
     }
-
-    // TODO: Re-enable when translation layer is implemented
-    // @Override
-    // protected RequestHandler getRequestHandler() {
-    //     return requestHandler;
-    // }
 
     @Override
     protected AgentCard getAgentCard() {
