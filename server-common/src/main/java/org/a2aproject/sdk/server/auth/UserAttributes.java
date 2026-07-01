1dc67ea43972f7cf38cc717189f1ab715394283a
Sh1bari <vova_krasnov_2004@mail.ru>
2026-07-02 02:03:33 +0300

    feat: expose programmatic auth wiring

---
+++ server-common/src/main/java/org/a2aproject/sdk/server/auth/UserAttributes.java
@@ -0,0 +1,12 @@
+package org.a2aproject.sdk.server.auth;
+
+public final class UserAttributes {
+
+    public static final String AUTHORITIES = "authorities";
+    public static final String SCOPES = "scopes";
+    public static final String TENANT_ID = "tenantId";
+    public static final String CLIENT_ID = "clientId";
+
+    private UserAttributes() {
+    }
+}
